776d636eb79b62b5a1f9276f8ff6e711f34824c1
Cristian Cordova <cristiancordova177@gmail.com>
2026-04-10 16:46:56 -0600

    Initial Commit

---
+++ RemoteTvApp/RemoteTvAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteTvAppApp.swift
+//  RemoteTvApp
+//
+//  Created by Cristian Cordova on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct RemoteTvAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
