cfb12077663f24159e977cd2affb160e2c3c8dae
Cristian Cordova <cristiancordova177@gmail.com>
2026-04-22 14:17:22 -0600

    Create initial app structure with models, views, services, and view models

---
+++ RemoteTvApp/Models/TVDevice.swift
@@ -0,0 +1,8 @@
+//
+//  TVDevice.swift
+//  RemoteTvApp
+//
+//  Created by Cristian Cordova on 22/04/26.
+//
+
+import Foundation
---
+++ RemoteTvApp/Views/DeviceListView.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceListView.swift
+//  RemoteTvApp
+//
+//  Created by Cristian Cordova on 22/04/26.
+//
+
+import SwiftUI
+
+struct DeviceListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceListView()
+}
